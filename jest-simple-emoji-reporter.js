b5e715604f656c64e982b62343c7a21f0cc1ef51
RyanCraigMartin <ryan@ryanmartin.io>
2020-01-16 00:19:51 -0500

    chore: refactor reporter
    This PR includes the following: 	
    
    * A refactor of the reporter.
    * An update to the license's date and package version.

--- jest-simple-emoji-reporter.js
+++ jest-simple-emoji-reporter.js
@@ -1,70 +1,58 @@
+const log = console.log
+const stdout = process.stdout
 const pluralize = (word, count) => `${count} ${word}${count === 1 ? "" : "s"}`;
 
-class JestSimpleEmojiReporter {
-  constructor(globalConfig, options) {
-    this._globalConfig = globalConfig;
-    this._options = options;
-  }
+module.exports = class JestSimpleEmojiReporter {
+  constructor(_, options) { this._options = options; }
 
   onRunStart(test) {
-    console.log("🃏");
-    console.log(
-      pluralize("Test suite", test.numTotalTestSuites) + " were found...📝"
-    );
-    process.stdout.write("\n");
+    log("🃏");
+    log(pluralize("Test suite", test.numTotalTestSuites) + " were found...📝");
+    stdout.write("\n");
   }
 
-  onRunComplete(test, results) {
+  onTestResult(_, testResult) {
+    const testSuite = testResult
+    const logResult = (testStatus, testTitle) => ({
+      passed: log(`✅ - ${testTitle}`),
+      pending: log(`🔃 - ${testTitle}`),
+    }[testStatus])
+
+    testSuite.testResults.forEach(result => (
+      result.status === 'failed'
+        ? log(`❌ - ${result.title}`)
+        : logResult(result.status, result.title)
+    ))
+  }
+
+  onRunComplete(_, results) {
     const {
       numFailedTests,
       numPassedTests,
       numPendingTests,
       testResults,
       numTotalTests,
-      startTime
+      startTime,
     } = results;
 
-    process.stdout.write("\n");
-
-    testResults.map(({ failureMessage }) => {
-      if (failureMessage) {
-        console.log(failureMessage);
-      }
-    });
+    stdout.write("\n");
+    testResults.map(({ failureMessage }) => { if (failureMessage) log(failureMessage); });
 
     if (!results.snapshot.didUpdate && results.snapshot.unchecked) {
-      const obsoleteError =
-        pluralize("obsolete snapshot", results.snapshot.unchecked) + " found.";
-      if (this._options.color) console.log(`\x1b[31m${obsoleteError}\x1b[0m`);
-      else console.log(obsoleteError);
+      const obsoleteError = pluralize("obsolete snapshot", results.snapshot.unchecked) + " found.";
+      this._options.color ? log(`\x1b[31m${obsoleteError}\x1b[0m`) : log(obsoleteError);
     }
 
-    console.log(`Ran ${numTotalTests} tests in ${testDuration()}`);
-    process.stdout.write(`\x1b[32m${numPassedTests || 0} passing\x1b[32m`);
-    process.stdout.write("\n");
-    process.stdout.write(`\x1b[31m${numFailedTests || 0} failing\x1b[31m`);
-    process.stdout.write("\n");
-    process.stdout.write(`\x1b[33m${numPendingTests || 0} pending\x1b[33m`);
-    process.stdout.write("\n");
-
-    function testDuration() {
+    const testDuration = () => {
       const end = new Date();
       const start = new Date(startTime);
       const sec = (end - start) / 1000;
       return `${sec}s`;
     }
-  }
 
-  onTestResult(test, testResult) {
-    for (let i = 0; i < testResult.testResults.length; i++) {
-      if (testResult.testResults[i].status === "passed") {
-        process.stdout.write("  ✅");
-      } else if (testResult.testResults[i].status === "pending") {
-        process.stdout.write("  🔃");
-      } else {
-        process.stdout.write("  ❌");
-      }
-    }
+    log(`Ran ${numTotalTests} tests in ${testDuration()}`);
+    log(`\x1b[32m${numPassedTests || 0} passing\x1b[32m`); stdout.write("\n");
+    log(`\x1b[31m${numFailedTests || 0} failing\x1b[31m`); stdout.write("\n");
+    log(`\x1b[33m${numPendingTests || 0} pending\x1b[33m`); stdout.write("\n");
   }
 }
-module.exports = JestSimpleEmojiReporter;
